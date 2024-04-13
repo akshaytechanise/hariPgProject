174a1ddfca2e9fe92592ad7ae412624cad3cfd7f
Harikrishnan s <harikrishnansivadasan@gmail.com>
2024-04-14 01:07:02 +0530

    actualmmodel.runsyn/use-camerahook/prediction-part/path to give

--- src/screens/CameraScreen/useCameraHook.ts
+++ src/screens/CameraScreen/useCameraHook.ts
@@ -43,16 +43,38 @@ const useCameraHook = () => {
       console.log(`Running inference on ${frame}`);
       const config: any = {
         scale: {
-          width: 320,
-          height: 320,
+          width: 224,
+          height: 224,
         },
         pixelFormat: 'rgb',
-        dataType: 'uint8',
+        dataType: 'float32',
       };
       const resized = resize(frame, config);
       const result = actualModel.runSync([resized]);
-      const num_detections = result[3]?.[0] ?? 0;
-      console.log('Result: ' + num_detections);
+      // const num_detections = result[3]?.[0] ?? 0;
+      // console.log('Result: ' + num_detections);
+      const outputTensor = result[0];
+      // Define your class labels array
+      const classLabels: string[] = ["NORMAL", "PRE-CANCER", "ORAL-CANCER"]; // Replace with your actual class labels
+
+      // Find the maximum value and its index
+      let maxIndex = 0;
+      let maxValue = outputTensor[0];
+      for (let i = 1; i < outputTensor.length; i++) {
+        if (outputTensor[i] > maxValue) {
+          maxIndex = i;
+          maxValue = outputTensor[i];
+        }
+      }
+
+      // Get the corresponding class label
+      const maxClass = classLabels[maxIndex];
+
+      console.log("Confidence:", maxValue);
+      //console.log("Index of maximum value:", maxIndex);
+      console.log("prediction:", maxClass);
+      // console.log('FINAL RESULT', Alldetections);
+      console.log('out', outputTensor)
     },
     [actualModel],
   );
